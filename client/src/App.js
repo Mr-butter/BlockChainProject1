b711c79b594904489a6ea1118bd20eba3e13d300
Mr-butter <89626632+Mr-butter@users.noreply.github.com>
2022-01-06 13:09:34 +0900

    server and client connect to proxy

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,15 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-    function test() {
-        axios.get("/").then((res) => {
-            console.log(res.data.say);
-        });
-    }
-    test();
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <button onClick={()=>{
+                    axios.post("/").then((res) => {
+                        console.log(res.data.say);
+                    });
+                }}>프록시 연결 테스트</button>
             </header>
         </div>
     );
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.post("/", function (req, res, next) {
     res.json({ say: "안녕리액트야" });
 });
 
