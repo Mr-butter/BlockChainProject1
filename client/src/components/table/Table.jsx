55498a4f7e035950b240b25387e52ea206ed8332
Rina <fghjiff8@gmail.com>
2022-01-09 23:03:04 +0900

    table

--- client/src/components/table/Table.jsx
+++ client/src/components/table/Table.jsx
@@ -5,7 +5,26 @@ import "./table.css";
 const Table = (props) => {
   return (
     <div>
-      <>table</>
+      <div className="table-wrapper">
+        <table>
+          {props.headData && props.renderHead ? (
+            <thead>
+              <tr>
+                {props.headData.map((item, index) =>
+                  props.renderHead(item, index)
+                )}
+              </tr>
+            </thead>
+          ) : null}
+          {props.bodyData && props.renderBody ? (
+            <tbody>
+              {props.bodyData.map((item, index) =>
+                props.renderBody(item, index)
+              )}
+            </tbody>
+          ) : null}
+        </table>
+      </div>
     </div>
   );
 };
