e93c4e2d970ce7263c09ae5409f9e86292406d98
Rina <fghjiff8@gmail.com>
2022-01-10 13:02:01 +0900

    top nav 유저 아이콘 수정과  theme 추가

---
+++ client/src/components/themeMenu/ThemeMenu.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import "./thememenu.css";
+
+const mode_setting = [
+  {
+    id: "light",
+    name: "Light",
+    background: "light-background",
+    class: "theme-mode-light",
+    logo: "",
+  },
+  {
+    id: "dark",
+    name: "Dark",
+    background: "dark-background",
+    class: "theme-mode-dark",
+    logo: "",
+  },
+];
+
+const color_setting = "";
+
+const ThemeMenu = () => {
+  return (
+    <div>
+      <button className="dropdown__toggle">
+        <i className="bx bx-palette"></i>
+      </button>
+      <div className="theme-menu">
+        <h4>Theme setting</h4>
+        <button className="theme-menu__close">
+          <i className="bx bx-x"></i>
+        </button>
+        <div className="theme-menu__select">
+          <span>Choose mode</span>
+          <ul className="mode-list">
+            {mode_setting.map((item, index) => (
+              <li key={index}>
+                <div className={`mode-list__color ${item.background}`}>
+                    <i className="bx bx-check"
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ThemeMenu;
