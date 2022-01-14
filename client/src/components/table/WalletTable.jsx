2acd6b9e2f914294cc3da88566c52d0319e451c9
Rina <fghjiff8@gmail.com>
2022-01-14 17:15:23 +0900

    wallet page

--- client/src/components/table/WalletTable.jsx
+++ client/src/components/table/WalletTable.jsx
@@ -1,10 +1,54 @@
 import React from "react";
+import "./wallettable.css";
 
 const WalletTable = () => {
   return (
     <div>
-      <div className="table-wrapper">
-        <table></table>
+      <div className="wallet-wrapper">
+        <h3>This is Your Address</h3>
+        <br />
+        <table>
+          <thead>
+            <tr>
+              <th>My Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>fdgdhgfsasd</td>
+            </tr>
+          </tbody>
+        </table>
+        <br />
+        <h3>This is Your Public key</h3>
+        <br />
+        <table>
+          <thead>
+            <tr>
+              <th>Public key</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>fdgdhgfsasd</td>
+            </tr>
+          </tbody>
+        </table>
+        <br />
+        <h3>This is Your Private Key</h3>
+        <br />
+        <table>
+          <thead>
+            <tr>
+              <th>PrivateKeyHex</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>fdgdhgfsasd</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
