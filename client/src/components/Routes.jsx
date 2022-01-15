ba95f6514a814de24bb8b4a42ca4f8cd792dddbc
Rina <fghjiff8@gmail.com>
2022-01-15 11:33:28 +0900

    mining page

--- client/src/components/Routes.jsx
+++ client/src/components/Routes.jsx
@@ -4,18 +4,20 @@ import { Route, Switch } from "react-router-dom";
 
 import Dashboard from "../Pages/Dashboard";
 import BlockDetail from "../Pages/BlockDetail";
+import Mining from "../Pages/Mining";
 import Mypage from "../Pages/Mypage";
 import Testingboard from "../Pages/Testingboard";
 
 const Routes = () => {
-    return (
-        <Switch>
-            <Route path="/" exact component={Dashboard} />
-            <Route path="/analytics" component={BlockDetail} />
-            <Route path="/mypage" component={Mypage} />
-            <Route path="/testing" component={Testingboard} />
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route path="/" exact component={Dashboard} />s
+      <Route path="/analytics" component={BlockDetail} />
+      <Route path="/mining" component={Mining} />
+      <Route path="/mypage" component={Mypage} />
+      <Route path="/testing" component={Testingboard} />
+    </Switch>
+  );
 };
 
 export default Routes;
