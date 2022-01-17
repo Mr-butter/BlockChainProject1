e27fcabf38f813fb04416fac3ffe4535e2fce415
Mr-butter <89626632+Mr-butter@users.noreply.github.com>
2022-01-17 17:48:00 +0900

    edit code

--- server/public/chainedBlock.js
+++ server/public/chainedBlock.js
@@ -3,102 +3,19 @@ const merkle = require("merkle");
 const cryptojs = require("crypto-js");
 const random = require("random");
 const { WebSocket } = require("ws");
-const p2pServer_func = require("./p2pServer");
 
 const BLOCK_GENERATION_INTERVAL = 10; //단위시간 초
 const DIIFFICULTY_ADJUSTMENT_INTERVAL = 10;
 
 class Block {
-    constructor(header, body) {
-        this.header = header;
-        this.body = body;
-    }
+  constructor(header, body) {
+    this.header = header;
+    this.body = body;
+  }
 }
 
 class BlockHeader {
-    constructor(
-        version,
-        index,
-        previousHash,
-        timestamp,
-        merkleRoot,
-        difficulty,
-        nonce
-    ) {
-        this.version = version;
-        this.index = index;
-        this.previousHash = previousHash;
-        this.timestamp = timestamp;
-        this.merkleRoot = merkleRoot;
-        this.difficulty = difficulty;
-        this.nonce = nonce;
-    }
-}
-
-function getVersion() {
-    const package = fs.readFileSync("package.json");
-    return JSON.parse(package).version;
-}
-
-function createGenesisBlock() {
-    const version = getVersion();
-    const index = 0;
-    const previousHash = "0".repeat(64);
-    const timestamp = Math.round(new Date().getTime() / 1000);
-
-    const body = [
-        "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks",
-    ];
-    const tree = merkle("sha256").sync(body);
-    const merkleRoot = tree.root() || "0".repeat(64);
-    const difficulty = 0;
-    const nonce = 0;
-
-    const header = new BlockHeader(
-        version,
-        index,
-        previousHash,
-        timestamp,
-        merkleRoot,
-        difficulty,
-        nonce
-    );
-    return new Block(header, body);
-}
-
-let Blocks = [createGenesisBlock()];
-
-function getBlocks() {
-    return Blocks;
-}
-
-function getLastBlock() {
-    return Blocks[Blocks.length - 1];
-}
-
-function createHash(block) {
-    const {
-        version,
-        index,
-        previousHash,
-        timestamp,
-        merkleRoot,
-        difficulty,
-        nonce,
-    } = block.header;
-    const blockString =
-        version +
-        index +
-        previousHash +
-        timestamp +
-        merkleRoot +
-        difficulty +
-        nonce;
-    const hash = cryptojs.SHA256(blockString).toString();
-    return hash;
-}
-
-function calculateHash(
+  constructor(
     version,
     index,
     previousHash,
@@ -106,281 +23,364 @@ function calculateHash(
     merkleRoot,
     difficulty,
     nonce
+  ) {
+    this.version = version;
+    this.index = index;
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+    this.merkleRoot = merkleRoot;
+    this.difficulty = difficulty;
+    this.nonce = nonce;
+  }
+}
+
+function getVersion() {
+  const package = fs.readFileSync("package.json");
+  return JSON.parse(package).version;
+}
+
+function createGenesisBlock() {
+  const version = getVersion();
+  const index = 0;
+  const previousHash = "0".repeat(64);
+  const timestamp = Math.round(new Date().getTime() / 1000);
+
+  const body = [
+    "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks",
+  ];
+  const tree = merkle("sha256").sync(body);
+  const merkleRoot = tree.root() || "0".repeat(64);
+  const difficulty = 0;
+  const nonce = 0;
+
+  const header = new BlockHeader(
+    version,
+    index,
+    previousHash,
+    timestamp,
+    merkleRoot,
+    difficulty,
+    nonce
+  );
+  return new Block(header, body);
+}
+
+let Blocks = [createGenesisBlock()];
+
+function getBlocks() {
+  return Blocks;
+}
+
+function getLastBlock() {
+  return Blocks[Blocks.length - 1];
+}
+
+function createHash(block) {
+  const {
+    version,
+    index,
+    previousHash,
+    timestamp,
+    merkleRoot,
+    difficulty,
+    nonce,
+  } = block.header;
+  const blockString =
+    version +
+    index +
+    previousHash +
+    timestamp +
+    merkleRoot +
+    difficulty +
+    nonce;
+  const hash = cryptojs.SHA256(blockString).toString();
+  return hash;
+}
+
+function calculateHash(
+  version,
+  index,
+  previousHash,
+  timestamp,
+  merkleRoot,
+  difficulty,
+  nonce
 ) {
-    const blockString =
-        version +
-        index +
-        previousHash +
-        timestamp +
-        merkleRoot +
-        difficulty +
-        nonce;
-    const hash = cryptojs.SHA256(blockString).toString();
-    return hash;
+  const blockString =
+    version +
+    index +
+    previousHash +
+    timestamp +
+    merkleRoot +
+    difficulty +
+    nonce;
+  const hash = cryptojs.SHA256(blockString).toString();
+  return hash;
 }
 
 const genesisBlock = createGenesisBlock();
 
 function nextBlock(bodyData) {
-    const { broadcast, responseLatestMsg } = require("./p2pServer");
-    const prevBlock = getLastBlock();
+  const { broadcast, responseLatestMsg } = require("./p2pServer");
+  const prevBlock = getLastBlock();
 
-    const version = getVersion();
-    const index = prevBlock.header.index + 1;
-    const previousHash = createHash(prevBlock);
-    const timestamp = Math.round(new Date().getTime() / 1000);
-    const tree = merkle("sha256").sync(bodyData);
-    const merkleRoot = tree.root() || "0".repeat(64);
-    const difficulty = getDifficulty(getBlocks());
+  const version = getVersion();
+  const index = prevBlock.header.index + 1;
+  const previousHash = createHash(prevBlock);
+  const timestamp = Math.round(new Date().getTime() / 1000);
+  const tree = merkle("sha256").sync(bodyData);
+  const merkleRoot = tree.root() || "0".repeat(64);
+  const difficulty = getDifficulty(getBlocks());
 
-    const newBlock = findBlock(
-        version,
-        index,
-        previousHash,
-        timestamp,
-        merkleRoot,
-        difficulty,
-        bodyData
-    );
-    if (isValidNewBlock(newBlock, prevBlock)) {
-        broadcast(responseLatestMsg());
-        return newBlock;
-    } else {
-        return null;
-    }
-}
-
-function replaceChain(newBlocks) {
-    const { broadcast, responseLatestMsg } = require("./p2pServer");
-
-    if (isValidChain(newBlocks)) {
-        if (
-            newBlocks.length > Blocks.length ||
-            (newBlocks.length === Blocks.length && random.boolean())
-        ) {
-            Blocks = newBlocks;
-            broadcast(responseLatestMsg());
-        }
-    } else {
-        console.log("받은 원장에 문제가 있음");
-    }
-}
-
-function hexToBinary(s) {
-    const lookupTable = {
-        0: "0000",
-        1: "0001",
-        2: "0010",
-        3: "0011",
-        4: "0100",
-        5: "0101",
-        6: "0110",
-        7: "0111",
-        8: "1000",
-        9: "1001",
-        A: "1010",
-        B: "1011",
-        C: "1100",
-        D: "1101",
-        E: "1110",
-        F: "1111",
-    };
-
-    var ret = "";
-    for (var i = 0; i < s.length; i++) {
-        if (lookupTable[s[i]]) {
-            ret += lookupTable[s[i]];
-        } else {
-            return null;
-        }
-    }
-    return ret;
-}
-
-function hashMatchesDifficulty(hash, difficulty) {
-    const hashBinary = hexToBinary(hash.toUpperCase());
-    const requirePrefix = "0".repeat(difficulty);
-    return hashBinary.startsWith(requirePrefix);
-}
-
-function findBlock(
-    currentVersion,
-    nextIndex,
+  const newBlock = findBlock(
+    version,
+    index,
     previousHash,
-    nextTimestamp,
+    timestamp,
     merkleRoot,
     difficulty,
     bodyData
-) {
-    var nonce = 0;
+  );
+  if (isValidNewBlock(newBlock, prevBlock)) {
+    broadcast(responseLatestMsg());
+    return newBlock;
+  } else {
+    return null;
+  }
+}
 
-    while (true) {
-        var hash = calculateHash(
-            currentVersion,
-            nextIndex,
-            previousHash,
-            nextTimestamp,
-            merkleRoot,
-            difficulty,
-            nonce
-        );
-        if (hashMatchesDifficulty(hash, difficulty)) {
-            const newBlockHeader = new BlockHeader(
-                currentVersion,
-                nextIndex,
-                previousHash,
-                nextTimestamp,
-                merkleRoot,
-                difficulty,
-                nonce
-            );
+function replaceChain(newBlocks) {
+  const { broadcast, responseLatestMsg } = require("./p2pServer");
 
-            return new Block(newBlockHeader, bodyData);
-        }
-        nonce++;
+  if (isValidChain(newBlocks)) {
+    if (
+      newBlocks.length > Blocks.length ||
+      (newBlocks.length === Blocks.length && random.boolean())
+    ) {
+      Blocks = newBlocks;
+      broadcast(responseLatestMsg());
     }
+  } else {
+    console.log("받은 원장에 문제가 있음");
+  }
+}
+
+function hexToBinary(s) {
+  const lookupTable = {
+    0: "0000",
+    1: "0001",
+    2: "0010",
+    3: "0011",
+    4: "0100",
+    5: "0101",
+    6: "0110",
+    7: "0111",
+    8: "1000",
+    9: "1001",
+    A: "1010",
+    B: "1011",
+    C: "1100",
+    D: "1101",
+    E: "1110",
+    F: "1111",
+  };
+
+  var ret = "";
+  for (var i = 0; i < s.length; i++) {
+    if (lookupTable[s[i]]) {
+      ret += lookupTable[s[i]];
+    } else {
+      return null;
+    }
+  }
+  return ret;
+}
+
+function hashMatchesDifficulty(hash, difficulty) {
+  const hashBinary = hexToBinary(hash.toUpperCase());
+  const requirePrefix = "0".repeat(difficulty);
+  return hashBinary.startsWith(requirePrefix);
+}
+
+function findBlock(
+  currentVersion,
+  nextIndex,
+  previousHash,
+  nextTimestamp,
+  merkleRoot,
+  difficulty,
+  bodyData
+) {
+  var nonce = 0;
+
+  while (true) {
+    var hash = calculateHash(
+      currentVersion,
+      nextIndex,
+      previousHash,
+      nextTimestamp,
+      merkleRoot,
+      difficulty,
+      nonce
+    );
+    if (hashMatchesDifficulty(hash, difficulty)) {
+      const newBlockHeader = new BlockHeader(
+        currentVersion,
+        nextIndex,
+        previousHash,
+        nextTimestamp,
+        merkleRoot,
+        difficulty,
+        nonce
+      );
+
+      return new Block(newBlockHeader, bodyData);
+    }
+    nonce++;
+  }
 }
 
 function getDifficulty(blocks) {
-    const lastBlock = blocks[blocks.length - 1];
-    if (
-        lastBlock.header.index !== 0 &&
-        lastBlock.header.index % DIIFFICULTY_ADJUSTMENT_INTERVAL === 0
-    ) {
-        return getAdjustDifficulty(lastBlock, blocks);
-    }
-    return lastBlock.header.difficulty;
+  const lastBlock = blocks[blocks.length - 1];
+  if (
+    lastBlock.header.index !== 0 &&
+    lastBlock.header.index % DIIFFICULTY_ADJUSTMENT_INTERVAL === 0
+  ) {
+    return getAdjustDifficulty(lastBlock, blocks);
+  }
+  return lastBlock.header.difficulty;
 }
 
 function getAdjustDifficulty(lastBlock, blocks) {
-    const prevAdjustmentBlock =
-        blocks[blocks.length - DIIFFICULTY_ADJUSTMENT_INTERVAL];
-    const elapsedTime =
-        lastBlock.header.timestamp - prevAdjustmentBlock.header.timestamp;
-    const expectedTime =
-        BLOCK_GENERATION_INTERVAL * DIIFFICULTY_ADJUSTMENT_INTERVAL;
-    if (expectedTime / 2 > elapsedTime) {
-        return prevAdjustmentBlock.header.difficulty + 1;
-    } else if (expectedTime * 2 < elapsedTime) {
-        return prevAdjustmentBlock.header.difficulty - 1;
-    } else {
-        return prevAdjustmentBlock.header.difficulty;
-    }
+  const prevAdjustmentBlock =
+    blocks[blocks.length - DIIFFICULTY_ADJUSTMENT_INTERVAL];
+  const elapsedTime =
+    lastBlock.header.timestamp - prevAdjustmentBlock.header.timestamp;
+  const expectedTime =
+    BLOCK_GENERATION_INTERVAL * DIIFFICULTY_ADJUSTMENT_INTERVAL;
+  if (expectedTime / 2 > elapsedTime) {
+    return prevAdjustmentBlock.header.difficulty + 1;
+  } else if (expectedTime * 2 < elapsedTime) {
+    return prevAdjustmentBlock.header.difficulty - 1;
+  } else {
+    return prevAdjustmentBlock.header.difficulty;
+  }
 }
 
 //////////////////////////// 검증코드
 function isValidBlockStructure(block) {
-    return (
-        typeof block.header.version === "string" &&
-        typeof block.header.index === "number" &&
-        typeof block.header.previousHash === "string" &&
-        typeof block.header.timestamp === "number" &&
-        typeof block.header.merkleRoot === "string" &&
-        typeof block.header.difficulty === "number" &&
-        typeof block.header.nonce === "number" &&
-        typeof block.body === "object"
-    );
+  return (
+    typeof block.header.version === "string" &&
+    typeof block.header.index === "number" &&
+    typeof block.header.previousHash === "string" &&
+    typeof block.header.timestamp === "number" &&
+    typeof block.header.merkleRoot === "string" &&
+    typeof block.header.difficulty === "number" &&
+    typeof block.header.nonce === "number" &&
+    typeof block.body === "object"
+  );
 }
 
 function isValidNewBlock(newBlock, previousBlock) {
-    if (isValidBlockStructure(newBlock) === false) {
-        console.log("Invalid Block Structure");
-        return false;
-    } else if (newBlock.header.index !== previousBlock.header.index + 1) {
-        console.log("Invalid Index");
-        return false;
-    } else if (createHash(previousBlock) !== newBlock.header.previousHash) {
-        console.log("Invalid previousHash");
-        return false;
-    } else if (
-        (newBlock.body.length === 0 &&
-            "0".repeat(64) !== newBlock.header.merkleRoot) ||
-        (newBlock.body.length !== 0 &&
-            merkle("sha256").sync(newBlock.body).root() !==
-                newBlock.header.merkleRoot)
-    ) {
-        console.log("Invalid merkleRoot");
-        return false;
-    } else if (!isValidTimestamp(newBlock, previousBlock)) {
-        console.log("invalid timestamp");
-        return false;
-    } else if (
-        !hashMatchesDifficulty(createHash(newBlock), newBlock.header.difficulty)
-    ) {
-        console.log("Invalid hash");
-        return false;
-    }
-    return true;
+  if (isValidBlockStructure(newBlock) === false) {
+    console.log("Invalid Block Structure");
+    return false;
+  } else if (newBlock.header.index !== previousBlock.header.index + 1) {
+    console.log("Invalid Index");
+    return false;
+  } else if (createHash(previousBlock) !== newBlock.header.previousHash) {
+    console.log("Invalid previousHash");
+    return false;
+  } else if (
+    (newBlock.body.length === 0 &&
+      "0".repeat(64) !== newBlock.header.merkleRoot) ||
+    (newBlock.body.length !== 0 &&
+      merkle("sha256").sync(newBlock.body).root() !==
+        newBlock.header.merkleRoot)
+  ) {
+    console.log("Invalid merkleRoot");
+    return false;
+  } else if (!isValidTimestamp(newBlock, previousBlock)) {
+    console.log("invalid timestamp");
+    return false;
+  } else if (
+    !hashMatchesDifficulty(createHash(newBlock), newBlock.header.difficulty)
+  ) {
+    console.log("Invalid hash");
+    return false;
+  }
+  return true;
 }
 
 function getCurrentTimestamp() {
-    return Math.round(new Date().getTime() / 1000);
+  return Math.round(new Date().getTime() / 1000);
 }
 
 function isValidTimestamp(newBlock, previousBlock) {
-    return (
-        previousBlock.header.timestamp - 60 < newBlock.header.timestamp &&
-        newBlock.header.timestamp - 60 < getCurrentTimestamp()
-    );
+  return (
+    previousBlock.header.timestamp - 60 < newBlock.header.timestamp &&
+    newBlock.header.timestamp - 60 < getCurrentTimestamp()
+  );
 }
 
 function isValidChain(newBlocks) {
-    if (JSON.stringify(newBlocsk[0]) !== JSON.stringify(Blocks[0])) {
-        return false;
-    }
+  if (JSON.stringify(newBlocks[0]) !== JSON.stringify(Blocks[0])) {
+    return false;
+  }
 
-    var tempBlocks = [newBlocks[0]];
-    for (var i = 1; i < newBlocks.length; i++) {
-        if (isValidNewBlock(newBlocks[i], tempBlocks[i - 1])) {
-            tempBlocks.push(newBlocks[i]);
-        } else {
-            return false;
-        }
+  var tempBlocks = [newBlocks[0]];
+  for (var i = 1; i < newBlocks.length; i++) {
+    if (isValidNewBlock(newBlocks[i], tempBlocks[i - 1])) {
+      tempBlocks.push(newBlocks[i]);
+    } else {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 //////////////////////////////////////임의거래장부
 let transectionArry = [];
 
 setInterval(() => {
-    const transection = { addTransection: parseInt(Math.random() * 1000) };
-    transectionArry.push(transection);
+  const transection = { addTransection: parseInt(Math.random() * 1000) };
+  transectionArry.push(transection);
 }, Math.random() * 1000);
 //////////////////////////////////////
 
 function addBlock() {
-    const newBlock = nextBlock(transectionArry);
-    if (isValidNewBlock(newBlock, getLastBlock())) {
-        transectionArry = [];
-        Blocks.push(newBlock);
-        return newBlock;
-    }
-    return null;
+  console.log("채굴");
+  const newBlock = nextBlock(transectionArry);
+  if (isValidNewBlock(newBlock, getLastBlock())) {
+    transectionArry = [];
+    Blocks.push(newBlock);
+    return newBlock;
+  }
+  return null;
 }
 
 function minning(message) {
-    switch (message) {
-        case "on":
-            setInterval(() => addBlock(), 2000);
-            break;
-        case "off":
-            return process.exit();
+  switch (message) {
+    case "on":
+      setInterval(() => addBlock(), 1000);
+      break;
+    case "off":
+      return process.exit();
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 }
 
 module.exports = {
-    Blocks,
-    addBlock,
-    getLastBlock,
-    createHash,
-    nextBlock,
-    getVersion,
-    getBlocks,
-    hexToBinary,
-    hashMatchesDifficulty,
-    replaceChain,
-    minning,
+  Blocks,
+  addBlock,
+  getLastBlock,
+  createHash,
+  nextBlock,
+  getVersion,
+  getBlocks,
+  hexToBinary,
+  hashMatchesDifficulty,
+  replaceChain,
+  minning,
 };
