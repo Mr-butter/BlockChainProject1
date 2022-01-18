265fe032f82dcc42d8b2c824cc44a77b1ff62536
Rina <fghjiff8@gmail.com>
2022-01-18 10:02:49 +0900

    block디테일

--- client/src/components/walletModal/NewWallet.jsx
+++ client/src/components/walletModal/NewWallet.jsx
@@ -112,11 +112,11 @@ const NewWallet = (props) => {
           </Button>
         </Grid>
         <br />
-        <InputLabel shrink variant="filled">
+        {/* <InputLabel shrink variant="filled">
           니모닉 문구를 아는 사람 누구나 지갑에 접근이 가능하므로 안전하게
           보관바랍니다. This is the only way you will be able to recover your
           account. Please store it somewhere safe !
-        </InputLabel>
+        </InputLabel> */}
         <FormControl style={{ width: "5px" }}></FormControl>
 
         <p>
