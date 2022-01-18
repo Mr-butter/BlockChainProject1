dce3a8106f6061d280237690f10fadc364cc2e22
Rina <fghjiff8@gmail.com>
2022-01-18 18:45:39 +0900

    dbdbdb

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,35 +1,36 @@
 var express = require("express");
 var router = express.Router();
 const { WebSocket } = require("ws");
+const blcokChainDB = require("../models/blocks");
 
 router.post("/blocks", (req, res) => {
-    const chainedBlock_func = require("../public/chainedBlock");
-    const blocks = chainedBlock_func.getBlocks();
-    res.send(blocks);
+  const chainedBlock_func = require("../public/chainedBlock");
+  const blocks = chainedBlock_func.getBlocks();
+  res.send(blocks);
 });
 router.post("/inputport", (req, res) => {
-    const p2pServer_func = require("../public/p2pServer");
-    const port = req.body.port;
-    console.log(port);
-    p2pServer_func.connectToPeer(port);
-    res.send({ message: `${port}번 포트로 웹소켓 접속` });
+  const p2pServer_func = require("../public/p2pServer");
+  const port = req.body.port;
+  console.log(port);
+  p2pServer_func.connectToPeer(port);
+  res.send({ message: `${port}번 포트로 웹소켓 접속` });
 });
 
 router.post("/mineBlock", (req, res) => {
-    const chainedBlock_func = require("../public/chainedBlock");
-    const switchOnOff = req.body.switchOnOff;
-    chainedBlock_func.minning(switchOnOff);
-    res.send({ message: ` 웹소켓 접속` });
+  const chainedBlock_func = require("../public/chainedBlock");
+  const switchOnOff = req.body.switchOnOff;
+  chainedBlock_func.minning(switchOnOff);
+  res.send({ message: ` 웹소켓 접속` });
 });
 
 router.post("/getsocket", (req, res) => {
-    const p2pServer_func = require("../public/p2pServer");
-    res.send(p2pServer_func.getSockets());
+  const p2pServer_func = require("../public/p2pServer");
+  res.send(p2pServer_func.getSockets());
 });
 
 router.post("/version", (req, res) => {
-    const chainedBlock_func = require("../public/chainedBlock");
-    res.send(chainedBlock_func.getVersion());
+  const chainedBlock_func = require("../public/chainedBlock");
+  res.send(chainedBlock_func.getVersion());
 });
 
 module.exports = router;
