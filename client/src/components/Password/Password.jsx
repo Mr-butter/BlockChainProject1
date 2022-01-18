8092e34569741e382aea2a9feb5e5558db2088df
Mr-butter <89626632+Mr-butter@users.noreply.github.com>
2022-01-18 10:20:47 +0900

    testing..

--- client/src/components/Password/Password.jsx
+++ client/src/components/Password/Password.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import {
-  Avatar,
-  Button,
-  Checkbox,
-  FormControlLabel,
-  Grid,
-  Link,
-  Paper,
-  TextField,
-  Typography,
+    Avatar,
+    Button,
+    Checkbox,
+    FormControlLabel,
+    Grid,
+    Link,
+    Paper,
+    TextField,
+    Typography,
 } from "@material-ui/core";
 import LockOutlined from "@mui/icons-material/LockOutlined";
 
@@ -34,127 +34,124 @@ import { decryption } from "../../utils/decrypt";
 // }
 
 const Password = (props) => {
-  useEffect(() => {
-    console.log(props.haveWallet);
-    console.log(props.sethaveWallet);
-  }, [props]);
+    useEffect(() => {}, [props]);
 
-  const gridStyle = {
-    padding: 10,
-  };
+    const gridStyle = {
+        padding: 10,
+    };
 
-  const paperStyle = {
-    padding: 20,
-    height: 470,
-    width: 300,
-    margin: "10px auto",
-  };
+    const paperStyle = {
+        padding: 20,
+        height: 470,
+        width: 300,
+        margin: "10px auto",
+    };
 
-  const theme = createMuiTheme({
-    palette: {
-      type: "dark",
-    },
-  });
+    const theme = createMuiTheme({
+        palette: {
+            type: "dark",
+        },
+    });
 
-  const avatarStyle = { backgroundColor: "gold" };
+    const avatarStyle = { backgroundColor: "gold" };
 
-  const btnstyle = { margin: "20px 5px" };
+    const btnstyle = { margin: "20px 5px" };
 
-  const [WalletPwdFromUser, setWalletPwdFromUser] = useState("");
+    const [WalletPwdFromUser, setWalletPwdFromUser] = useState("");
 
-  function getWalletPwdFromUser(event) {
-    setWalletPwdFromUser(event.currentTarget.value);
-  }
+    function getWalletPwdFromUser(event) {
+        setWalletPwdFromUser(event.currentTarget.value);
+    }
 
-  function getWallet() {
-    // setWalletPwdFromUser(event.currentTarget.value);
-    console.log(WalletPwdFromUser);
+    function getWallet() {
+        // setWalletPwdFromUser(event.currentTarget.value);
+        console.log(WalletPwdFromUser);
 
-    const loglevel = localStorage.getItem("loglevel");
-    console.log("로컬스토리지 client 확인 : ", loglevel);
+        const loglevel = localStorage.getItem("loglevel");
+        console.log("로컬스토리지 client 확인 : ", loglevel);
 
-    const dec = decryption(loglevel);
-    console.log(dec);
+        const dec = decryption(loglevel);
+        console.log(dec);
 
-    axios
-      .post("/wallet/getWallet", {
-        password: WalletPwdFromUser,
-        keystore: loglevel,
-        decryption: dec,
-      })
-      .then((res) => {
-        const data = res.data;
-        console.log("받은 데이터 확인 : ", data);
-      });
-  }
+        axios
+            .post("/wallet/getWallet", {
+                password: WalletPwdFromUser,
+                keystore: loglevel,
+                decryption: dec,
+            })
+            .then((res) => {
+                const data = res.data;
+                console.log("받은 데이터 확인 : ", data);
+            });
+    }
 
-  return (
-    <Grid style={gridStyle}>
-      <Paper className={10} style={paperStyle} variant="outlined">
-        <Grid align="center">
-          <Avatar style={avatarStyle}>
-            <LockOutlined />
-          </Avatar>
-          <h2>My 간편 비밀번호</h2>
+    return (
+        <Grid style={gridStyle}>
+            <Paper className={10} style={paperStyle} variant="outlined">
+                <Grid align="center">
+                    <Avatar style={avatarStyle}>
+                        <LockOutlined />
+                    </Avatar>
+                    <h2>My 간편 비밀번호</h2>
+                </Grid>
+                <TextField
+                    onChange={getWalletPwdFromUser}
+                    value={WalletPwdFromUser}
+                    label="password"
+                    placeholder="Enter password"
+                    fullwidth
+                    required
+                    style={{ width: "250px", marginTop: "15px" }}
+                />
+                <br />
+                <FormControlLabel
+                    control={<Checkbox name="checkedB" color="primary" />}
+                    label="Remember me"
+                />
+                <br />
+                <Button
+                    onClick={getWallet}
+                    href="#"
+                    type="submit"
+                    color="gold"
+                    style={btnstyle}
+                    variant="contained"
+                    fullWidth
+                >
+                    SIGN IN
+                </Button>
+                <Typography>
+                    <Link onClick={() => props.sethaveWallet("forgot")}>
+                        Forgot password ?
+                    </Link>
+                </Typography>
+                <br />
+                {/* Create New Wallet */}
+                <Typography>
+                    Do you have an account ?
+                    <br />
+                    <ThemeProvider theme={theme}>
+                        <button
+                            id="password"
+                            value="password"
+                            className="bx bxs-wallet bx-tada"
+                            onClick={() => props.sethaveWallet("wallet")}
+                            style={{
+                                display: "flex",
+                                width: "15px",
+                                height: "15px",
+                                fontSize: "2.7rem",
+                                alignItems: "center",
+                                marginTop: "25px",
+                                marginLeft: "95px",
+                                color: "white",
+                            }}
+                        ></button>
+                    </ThemeProvider>
+                </Typography>
+            </Paper>
         </Grid>
-        <TextField
-          onChange={getWalletPwdFromUser}
-          value={WalletPwdFromUser}
-          label="password"
-          placeholder="Enter password"
-          fullwidth
-          required
-          style={{ width: "250px", marginTop: "15px" }}
-        />
-        <br />
-        <FormControlLabel
-          control={<Checkbox name="checkedB" color="primary" />}
-          label="Remember me"
-        />
-        <br />
-        <Button
-          onClick={getWallet}
-          href="#"
-          type="submit"
-          color="gold"
-          style={btnstyle}
-          variant="contained"
-          fullWidth
-        >
-          SIGN IN
-        </Button>
-        <Typography>
-          <Link onClick={() => props.sethaveWallet("forgot")}>
-            Forgot password ?
-          </Link>
-        </Typography>
-        <br />
-        {/* Create New Wallet */}
-        <Typography>
-          Do you have an account ?
-          <br />
-          <ThemeProvider theme={theme}>
-            <button
-              id="password"
-              value="password"
-              className="bx bxs-wallet bx-tada"
-              onClick={() => props.sethaveWallet("wallet")}
-              style={{
-                display: "flex",
-                width: "15px",
-                height: "15px",
-                fontSize: "2.7rem",
-                alignItems: "center",
-                marginTop: "25px",
-                marginLeft: "95px",
-                color: "white",
-              }}
-            ></button>
-          </ThemeProvider>
-        </Typography>
-      </Paper>
-    </Grid>
-  );
+    );
 };
 
 export default Password;
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,36 +1,35 @@
 var express = require("express");
 var router = express.Router();
 const { WebSocket } = require("ws");
-const chainedBlock_func = require("../public/chainedBlock");
-const p2pServer_func = require("../public/p2pServer");
-// const { isMainThread, Worker, parentPort } = require("worker_threads");
-// const worker = new Worker("./public/p2pServer.js");
 
 router.post("/blocks", (req, res) => {
-  const blocks = chainedBlock_func.getBlocks();
-  res.send(blocks);
+    const chainedBlock_func = require("../public/chainedBlock");
+    const blocks = chainedBlock_func.getBlocks();
+    res.send(blocks);
 });
 router.post("/inputport", (req, res) => {
-  const port = req.body.port;
-  console.log(port);
-  p2pServer_func.connectToPeer(port);
-  res.send({ message: `${port}번 포트로 웹소켓 접속` });
+    const p2pServer_func = require("../public/p2pServer");
+    const port = req.body.port;
+    console.log(port);
+    p2pServer_func.connectToPeer(port);
+    res.send({ message: `${port}번 포트로 웹소켓 접속` });
 });
 
 router.post("/mineBlock", (req, res) => {
-  const switchOnOff = req.body.switchOnOff;
-  const p2pPort = req.body.p2pport;
-  p2pServer_func.connectToPeer(p2pPort);
-  chainedBlock_func.minning(switchOnOff);
-  res.send({ message: `${p2pPort}번 포트로 웹소켓 접속` });
+    const chainedBlock_func = require("../public/chainedBlock");
+    const switchOnOff = req.body.switchOnOff;
+    chainedBlock_func.minning(switchOnOff);
+    res.send({ message: ` 웹소켓 접속` });
 });
 
 router.post("/getsocket", (req, res) => {
-  res.send(p2pServer_func.getSockets());
+    const p2pServer_func = require("../public/p2pServer");
+    res.send(p2pServer_func.getSockets());
 });
 
 router.post("/version", (req, res) => {
-  res.send(chainedBlock_func.getVersion());
+    const chainedBlock_func = require("../public/chainedBlock");
+    res.send(chainedBlock_func.getVersion());
 });
 
 module.exports = router;
