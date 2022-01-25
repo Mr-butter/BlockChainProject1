0e5ab09c5633ea33730d3f4bd7a2ed2b2f3b8ab2
Mr-butter <89626632+Mr-butter@users.noreply.github.com>
2022-01-25 13:47:01 +0900

    test transaction

--- server/public/wallet.js
+++ server/public/wallet.js
@@ -2,62 +2,74 @@ const { keystore } = require("eth-lightwallet");
 const fs = require("fs");
 const CryptoJS = require("crypto-js");
 const UserWallet = require("../models/userWallet");
-const dotenv = require("dotenv");
-dotenv.config();
 
 function encryption(data) {
-  const key = process.env.ENCRYPTION_KEY;
-  const iv = process.env.ENCRYPTION_IV;
-  console.log(key);
-  console.log(iv);
+    const key = "process.env.ENCRYPTION_KEY";
+    const iv = "process.env.ENCRYPTION_IV";
 
-  const keyutf = CryptoJS.enc.Utf8.parse(key);
-  //console.log("키유티에프:", keyutf);
-  const ivutf = CryptoJS.enc.Utf8.parse(iv);
-  //console.log("아이브이유티에프:", ivutf);
-
-  const encObj = CryptoJS.AES.encrypt(JSON.stringify(data), keyutf, {
-    iv: ivutf,
-  });
-  //console.log("key : toString(CryptoJS.enc.Utf8)" + encObj.key.toString(CryptoJS.enc.Utf8));
-  //console.log("iv : toString(CryptoJS.enc.Utf8)" + encObj.iv.toString(CryptoJS.enc.Utf8));
-  //console.log("salt : " + encObj.salt);
-  //console.log("ciphertext : " + encObj.ciphertext);
-
-  const encStr = encObj + "";
-  console.log("encStr : " + encStr);
-
-  return encStr;
+    const keyutf = CryptoJS.enc.Utf8.parse(key);
+    const ivutf = CryptoJS.enc.Utf8.parse(iv);
+    const encObj = CryptoJS.AES.encrypt(JSON.stringify(data), keyutf, {
+        iv: ivutf,
+    });
+    const encStr = encObj + "";
+    return encStr;
 }
-const wooooo = "과자";
-console.log(encryption(wooooo));
-console.log(decryption(encryption(wooooo)));
 
 function decryption(encStr) {
-  const key = process.env.ENCRYPTION_KEY;
-  const iv = process.env.ENCRYPTION_IV;
+    const key = "process.env.ENCRYPTION_KEY";
+    const iv = "process.env.ENCRYPTION_IV";
 
-  const keyutf = CryptoJS.enc.Utf8.parse(key);
-  //console.log("키유티에프:", keyutf);
-  const ivutf = CryptoJS.enc.Utf8.parse(iv);
-  //console.log("아이브이유티에프:", ivutf);
-
-  //CryptoJS AES 128 복호화
-  const decObj = CryptoJS.AES.decrypt(
-    { ciphertext: CryptoJS.enc.Base64.parse(encStr) },
-    keyutf,
-    { iv: ivutf }
-  );
-  //console.log("decObj가 나올라나", decObj);
-
-  const decStr = CryptoJS.enc.Utf8.stringify(decObj);
-  //const parsedDecStr = JSON.parse(decStr)
-  // console.log("decStr : " + decStr);
-
-  return decStr;
+    const keyutf = CryptoJS.enc.Utf8.parse(key);
+    const ivutf = CryptoJS.enc.Utf8.parse(iv);
+    const decObj = CryptoJS.AES.decrypt(
+        { ciphertext: CryptoJS.enc.Base64.parse(encStr) },
+        keyutf,
+        { iv: ivutf }
+    );
+    const decStr = CryptoJS.enc.Utf8.stringify(decObj);
+    return decStr;
 }
 
 const getmnemonic = () => {
-  const mnemonic = keystore.generateRandomSeed();
-  return mnemonic;
+    const mnemonic = keystore.generateRandomSeed();
+    return mnemonic;
 };
+const password = 1234;
+const seedPhrase =
+    "gap thunder keep only skill silent speak jewel salute fame ancient great";
+
+const getKeystore = (password, seedPhrase) => {
+    new keystore.createVault(
+        {
+            password: password,
+            seedPhrase: seedPhrase,
+            hdPathString: "m/0'/0'/0'",
+        },
+        function (err, ks) {
+            // Some methods will require providing the `pwDerivedKey`,
+            // Allowing you to only decrypt private keys on an as-needed basis.
+            // You can generate that value with this convenient method:
+            ks.keyFromPassword(password, function (err, pwDerivedKey) {
+                if (err) throw err;
+
+                // generate five new address/private key pairs
+                // the corresponding private keys are also encrypted
+                ks.generateNewAddress(pwDerivedKey, 1);
+                const addr = ks.getAddresses();
+                const userkeystore = ks.serialize();
+                console.log(addr);
+                console.log(userkeystore);
+
+                ks.passwordProvider = function (callback) {
+                    var pw = prompt("Please enter password", "Password");
+                    callback(null, pw);
+                };
+
+                // Now set ks as transaction_signer in the hooked web3 provider
+                // and you can start using web3 using the keys/addresses in ks!
+            });
+        }
+    );
+};
+console.log(getKeystore(password, seedPhrase));
